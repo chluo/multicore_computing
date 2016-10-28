698de3fed684a01f1134c9a684c2f3628d5a7c5c
Chunheng Luo <chunhengluo@outlook.com>
2016-10-28 18:29:38 -0500

    Q2 part a works. Part b first commit.

--- hw4/q2/q2.cu
+++ hw4/q2/q2.cu
@@ -239,7 +239,7 @@ __global__ void shmem_counter_kernel(int * array_i, int * cnt_matrix, int array_
     // only have 10 values 
     if (threadIdx.x < 10) {
         cnt_matrix[threadIdx.x * blockDim.x + blockIdx.x] = scnt[threadIdx.x]; 
-        printf("%d\n", scnt[threadIdx.x]); 
+        printf("%d\n", cnt_matrix[threadIdx.x * blockDim.x + blockIdx.x]); 
     }
 }
 
