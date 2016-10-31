ea4f2ec907bf4b8c8b10ed1bdfb4c236c4e69103
Chunheng Luo <chunhengluo@outlook.com>
2016-10-31 13:47:24 -0500

    Testing time complexity.

--- hw4/q3/run.bash
+++ hw4/q3/run.bash
@@ -3,6 +3,6 @@
 #SBATCH -o myGPU%j         # output and error file name (%j expands to jobID)
 #SBATCH -n 1               # total number of mpi tasks requested
 #SBATCH -p gpudev          # queue (partition) -- normal, development, etc.
-#SBATCH -t 00:02:00        # run time (hh:mm:ss) - 120 seconds
+#SBATCH -t 01:00:00        # run time (hh:mm:ss) - 3600 seconds
 #SBATCH -A EE-382C-EE361C-Multi
 ./cu_out 
