394286b2adcbeadb4f089e3b22f584b8d0c90617
Chunheng Luo <chunhengluo@outlook.com>
2016-10-31 11:33:25 -0500

    Testing time complexity.

--- hw4/q3/q3.cu
+++ hw4/q3/q3.cu
@@ -211,7 +211,7 @@ int * compact(int * array_i, int * num_odd, int array_size) {
     cudaThreadSynchronize(); 
     
     // TODO: debug 
-    cudaPeekAtLastError();
+    printf("%s\n", cudaGetErrorString(cudaPeekAtLastError()));
     int * debug = (int *)malloc(array_size * sizeof(int)); 
     cudaMemcpy(debug, array_index, array_size * sizeof(int), cudaMemcpyDeviceToHost); 
     print_file(debug, array_size, "./debug.txt"); 
